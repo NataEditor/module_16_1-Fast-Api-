ff98e0a8e8feefc286bf48a09f52ff340ceedaab
NataEditor <nako_al@mail.ru>
2024-12-25 14:13:28 +0300

    Update modul 16_1.py
    Исправлен

--- modul 16_1.py
+++ modul 16_1.py
@@ -4,20 +4,20 @@ app = FastAPI()
 
 
 @app.get("/")
-async def welcome() -> str:
-    return {"message": "Главная страница"}
+async def welcome() -> dict:
+    return "Главная страница"
 
 
 @app.get("/user/admin")
 async def welcome() -> str:
-    return {"message": "Вы вошли как администратор"}
+    return "Вы вошли как администратор"
 
 
 @app.get("/user/{user_id}")
 async def usid(user_id: str) -> str:
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+    return f"Вы вошли как пользователь № {user_id}"
 
 
 @app.get("/user/{user_name}/{age}")
 async def usinf(user_name: str, age: str) -> str:
-    return {"message": f"Информация о пользователе. Имя: {user_name}, Возраст: {age}"}
+    return f"Информация о пользователе. Имя: {user_name}, Возраст: {age}"

51835d4bd4276aad011e8f58dced362cf366d69d
NataEditor <nako_al@mail.ru>
2024-12-25 14:43:52 +0300

    Update modul 16_1.py
    ispr

--- modul 16_1.py
+++ modul 16_1.py
@@ -3,21 +3,21 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def welcome() -> dict:
-    return "Главная страница"
+@app.get('/')
+async def main_page():
+    return 'Главная страница'
 
 
-@app.get("/user/admin")
-async def welcome() -> str:
-    return "Вы вошли как администратор"
+@app.get('/user/admin')
+async def admin_page():
+    return 'Вы вошли как администратор'
 
 
-@app.get("/user/{user_id}")
-async def usid(user_id: str) -> str:
-    return f"Вы вошли как пользователь № {user_id}"
+@app.get('/user/{user_id}')
+async def user_page_id(user_id: str)
+    return f'Вы вошли как пользователь № {user_id}'
 
 
-@app.get("/user/{user_name}/{age}")
-async def usinf(user_name: str, age: str) -> str:
-    return f"Информация о пользователе. Имя: {user_name}, Возраст: {age}"
+@app.get('/user/{user_name}/{age}')
+async def user_page(user_name: str, age: str)
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
